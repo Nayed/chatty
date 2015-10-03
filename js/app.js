9bbde60354d4b83f15040efe61f186fdbab1e819
Nayed <nayed.saidali@gmail.com>
2015-10-03 23:45:15 +0200

    websocket

--- js/app.js
+++ js/app.js
@@ -5,15 +5,20 @@ let msg = document.getElementById("messagebox")
 
 let socket = new WebSocket("ws://127.0.0.1:2000")
 
+let open = false
+
 let addMessage = msg => {
     chat.innerHTML += "<p>" + msg + "</p"
 }
 
 msg.addEventListener('keypress', evt => {
-    if (evt.charCode != 13 || msg.value == "")
+    if (evt.charCode != 13)
         return;
 
     evt.preventDefault()
+
+    if (msg.value == "" || !open)
+        return;
 
     socket.send(JSON.stringify({
         msg: msg.value
@@ -23,3 +28,19 @@ msg.addEventListener('keypress', evt => {
 
     msg.value = ""
 })
+
+socket.onopen = function() {
+    open = true
+
+    addMessage("Connected")
+}
+
+socket.onmessage = function(evt) {
+    let data = JSON.parse(evt.data)
+    addMessage(data.msg)
+}
+
+socket.onclose = function() {
+    open = false
+    addMessage("Disconnected")
+}
